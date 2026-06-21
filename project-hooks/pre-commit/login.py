8b0c3abc5b3daacfbdb71f8643fdee902895b220
Student <silviya.yordanova91@gmail.com>
2026-06-21 17:36:41 +0300

    Make changes in pre-commit and add pre-push

--- project-hooks/pre-commit/login.py
+++
@@ -1,3 +0,0 @@
-def login(username, password):
-
-    return f"Login successful for user: {username} Please check your email."
---
+++ project-hooks/test_sample_calculator.py
@@ -0,0 +1,14 @@
+from sample_calculator import sum, substract
+
+def test_sum():
+    assert sum(1, 2) == 3
+    assert sum(-1, -1) == -2
+    assert sum(0, 0) == 0
+
+
+def test_substract():
+    assert substract(5, 3) == 2
+    assert substract(-1, -1) == 0
+    assert substract(0, 5) == -5
+
+
